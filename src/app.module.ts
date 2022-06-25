6e169d5c69e00405e2f79d82c6f4fe96d2e9b027
francisco <fr.betancourt@live.com>
2022-06-25 17:59:06 -0400

    Created order module and controller. Created auth guard.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ProductModule } from './product/product.module';
+import { OrderModule } from './order/order.module';
 
 @Module({
-  imports: [AuthModule, ProductModule],
+  imports: [AuthModule, ProductModule, OrderModule],
   controllers: [AppController],
   providers: [AppService],
 })
